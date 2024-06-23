b519d9f4c47255bde6894c1d42f6586281b3aeaa
VigneshSK17 <vigsk17@gmail.com>
2024-06-23 16:42:57 -0400

    Moved around pages in navbar to consolidate

--- app/__init__.py
+++ app/__init__.py
@@ -18,13 +18,9 @@ def about():
 def work():
     return render_template('work.html', title="Work Experience", active_page = 'work')
 
-@app.route('/hobbies')
-def hobbies():
-    return render_template('hobbies.html', title="Hobbies", active_page = 'hobbies')
-
-@app.route('/education')
-def education():
-    return render_template('education.html', title="Education", active_page = 'education')
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="projects", active_page = 'projects')
 
 @app.route('/contact')
 def contact():
