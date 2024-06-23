eb8bb7394fac6d41d41359aa1cf580626868f540
jimmyMsh <jimmymishan2004@gmail.com>
2024-06-22 21:34:19 -0400

    Added several new web pages with associated hyperlinks in the more dropdown

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Me")
+
+@app.route('/work')
+def work():
+    return render_template('work.html', title="Work Experience")
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies")
+
+@app.route('/education')
+def education():
+    return render_template('education.html', title="Education")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact")
+
+if __name__ == '__main__':
+    app.run(debug=True)
