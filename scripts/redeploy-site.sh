c4879c862af61445a1e7c313d9c2d86571c64315
Jimmy Mishan <jimmymishan2004@gmail.com>
2024-07-12 23:14:20 +0000

    Update redeploy-site.sh to use systemd service for deployment instead of tmux

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions
-tmux kill-server
-
 # Enter the project directory (use absolute path)
 cd ~/pe-portfolio-site || { echo "Failed to cd into pe-portfolio-site"; exit 1; }
 
 # Run the command to make sure the repo inside VPS has the latest changes
-git fetch && git reset origin/main --hard
+git fetch && git reset origin/main --hard || { echo "Failed to update repository"; exit 1; }
 
 # Activate the venv and install dependencies
-source site_env/bin/activate
-pip install -r requirements.txt
+source site_env/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
+pip install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
 
-# Start new detached tmux session with the command to start the server detached
-tmux new -s website_instance -d "cd ~/pe-portfolio-site && source site_env/bin/activate && flask run --host=0.0.0.0"
+# Restart the service that runs the website
+sudo systemctl restart myportfolio || { echo "Failed to restart myportfolio service"; exit 1; }
 
-# Print a message indicating the server has started
-echo "Flask server started in tmux session 'website_instance'."
+# Check that the service is running
+status=$(systemctl is-active myportfolio)
+if [ "$status" = "active" ]; then
+  echo "Startup Successful, service is running"
+else
+  echo "Service is not running"
+fi
 
