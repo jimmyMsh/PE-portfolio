c26ccd195861a913fcad53178caafa5246db7ef9
Jimmy Mishan <jimmymishan2004@gmail.com>
2024-07-24 21:30:45 -0400

    Update deploy script and docker compose for production

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -6,18 +6,16 @@ cd ~/pe-portfolio-site || { echo "Failed to cd into pe-portfolio-site"; exit 1; 
 # Run the command to make sure the repo inside VPS has the latest changes
 git fetch && git reset origin/main --hard || { echo "Failed to update repository"; exit 1; }
 
-# Activate the venv and install dependencies
-source site_env/bin/activate || { echo "Failed to activate virtual environment"; exit 1; }
-pip install -r requirements.txt || { echo "Failed to install dependencies"; exit 1; }
+# Spin containers down to prevent out of memory issues on VPS instances when building
+docker compose -f docker-compose.prod.yml down || { echo "Failed to spin containers down"; exit 1; }
 
-# Restart the service that runs the website
-sudo systemctl restart myportfolio || { echo "Failed to restart myportfolio service"; exit 1; }
+# Build and run the containers
+docker compose -f docker-compose.prod.yml up -d --build || { echo "Failed to build and run containers"; exit 1; }
 
-# Check that the service is running
-status=$(systemctl is-active myportfolio)
-if [ "$status" = "active" ]; then
-  echo "Startup Successful, service is running"
+# Check that the Docker containers are running
+containers_status=$(docker ps -q)
+if [ -n "$containers_status" ]; then
+  echo "Deployment Successful, containers are running"
 else
-  echo "Service is not running"
+  echo "Containers are not running"
 fi
-
